35fe8520789d378ec1b0847dec473311d51ed4a0
Shani Riven <shani.riven@moveo.co.il>
2025-12-15 14:01:39 +0200

    mobile ui fix

--- frontend/components/card.tsx
+++ frontend/components/card.tsx
@@ -22,18 +22,18 @@ export type CardProps = {
 
 export function Card(props: CardProps) {
     return (
-        <article className={clsx("w-[245px] h-[232px]", "md:w-[379px] md:h-[405px]", "overflow-hidden bg-secondary-100")}>
+        <article className={clsx("w-[334px] h-[288px] bg-secondary-100", "md:w-[379px] md:h-[405px]",)}>
 
             {props.type === "restaurant" && props.chef && typeof props.rating === "number" && (
                 <>
-                    <div className={clsx("relative w-full h-[153px]", "md:h-[236px]")}>
+                    <div className={clsx("relative w-full h-[207px]", "md:h-[236px]")}>
                         <Image src={props.image} alt={props.name} fill className="object-cover" />
                     </div>
 
-                    <div className={clsx("p-4 ml-1 space-y-1 text-left", "md:text-center md:mt-3 md:space-y-3",)}>
+                    <div className={clsx("p-[16px] h-[82px] space-y-[8px] text-left", "md:text-center md:mt-3 md:space-y-3",)}>
 
                         <h3 className={clsx("md:text-[40px]")}>{props.name}</h3>
-                        <p className="md:text-[24px]">{props.chef}</p>
+                        <h2>{props.chef}</h2>
 
                         <div className="hidden md:flex justify-center">
                             <div className="flex items-center text-sm">
