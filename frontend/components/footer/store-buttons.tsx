0514b60932ebaa0e4baf4905a71762754dcc25f6
Shani Riven <shani.riven@moveo.co.il>
2025-12-15 17:04:55 +0200

    store-buttons platform change to enum

--- frontend/components/footer/store-buttons.tsx
+++ frontend/components/footer/store-buttons.tsx
@@ -1,6 +1,11 @@
 import Image from "next/image";
 import clsx from "clsx";
 
+enum StorePlatform {
+    GOOGLE = "google",
+    APPLE = "apple",
+}
+
 const STORE_CONFIG = {
     google: {
         src: "/icons/google-play.png",
@@ -15,7 +20,7 @@ const STORE_CONFIG = {
 }
 
 
-function StoreButton({ platform }: { platform: "google" | "apple" }) {
+function StoreButton({ platform }: { platform: StorePlatform }) {
     const store = STORE_CONFIG[platform];
 
     return (
@@ -35,8 +40,8 @@ export default function StoreButtons() {
     return (
         <>
             <div className="flex flex-col gap-4 mt-[32px] md:flex md:flex-row md:gap-4 md:mt-4">
-                <StoreButton platform="google" />
-                <StoreButton platform="apple" />
+                <StoreButton platform={StorePlatform.GOOGLE} />
+                <StoreButton platform={StorePlatform.APPLE} />
             </div>
         </>
     )
