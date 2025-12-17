490beb20a72ababef227cb6eff17c1fd9b74fb58
Shani Riven <shani.riven@moveo.co.il>
2025-12-17 13:07:22 +0200

    fixed props

--- frontend/app/(root)/home/page.tsx
+++ frontend/app/(root)/home/page.tsx
@@ -1,10 +1,9 @@
 import Hero from "@/components/hero"
 
-
 const HomePage = () => {
   return (
     <div className="w-full">
-      <Hero />
+      <Hero title={"Epicure works with the top \n chef restaurants in Tel Aviv"} imageSrc={"/images/hero-food.png"} />
     </div >
   )
 }
--- frontend/components/hero.tsx
+++ frontend/components/hero.tsx
@@ -2,14 +2,11 @@ import Image from "next/image";
 import SearchBox from "./search-box";
 
 type HeroProps = {
-    title?: string;
-    placeholder?: string;
-    imageSrc?: string;
+    title: string;
+    imageSrc: string;
 };
 
-export default function Hero({
-    imageSrc = "/images/hero-food.png",
-}: HeroProps) {
+export default function Hero({ title, imageSrc }: HeroProps) {
     return (
         <section >
             <div className="relative w-full overflow-hidden">
@@ -26,11 +23,8 @@ export default function Hero({
 
                 <div className="absolute inset-0 flex items-center justify-center px-4">
                     <div className="h-[144px] w-[335px] flex flex-col justify-center items-center gap-[18px] bg-white/[0.88] backdrop-blur-[0px] px-[9px] py-[36px] md:w-[776px] md:h-[221px] md:px-[126px] md:py-[36px]">
-                        <h1 className="text-center whitespace-pre-line">
-                            {
-                                `Epicure works with the top
-                                chef restaurants in Tel Aviv`
-                            }
+                        <h1 className="text-center">
+                            {title}
                         </h1>
                         <SearchBox />
                     </div>
